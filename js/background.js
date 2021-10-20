799727fee22f280d71f3fe4c780885fef8942a11
Patrick Cameguing <patrick.angelo@cameguing.com>
2021-10-21 01:22:56 +0800

    Adjust time schedule

--- js/background.js
+++ js/background.js
@@ -87,7 +87,7 @@ let fightNumber = 1;
 let shifts = [
     { starts: '08:34:00 AM', ends: '11:30:00 AM' },
     { starts: '03:48:00 PM', ends: '05:30:00 PM' },
-    { starts: '01:04:00 AM', ends: '03:30:00 AM' },
+    { starts: '01:07:00 AM', ends: '03:30:00 AM' },
 ];
 
 function createWebSocketConnection(crfToken) {
